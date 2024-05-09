7e8af8d1572cb95de0abbfe1957492e5a1e35c97
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-05-09 02:50:14 -0500

    Update App

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
             url = uri("https://maven.pkg.github.com/iZettle/sdk-android")
             credentials {
                 username = "vlzdtecnopac"
-                password = System.getenv("ZETTLE_SDK_GITHUB_ACCESS_TOKEN")
+                password = System.getenv("ZETTLE_SDK_GITHUB_ACCESS_TOKEN") ?: ""
             }
         }
 
