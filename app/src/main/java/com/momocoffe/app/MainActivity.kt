5c3828069d6372ced6f0ed2b21cb521bace5dba9
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-05-09 20:32:47 -0500

    Update Android

--- app/src/main/java/com/momocoffe/app/MainActivity.kt
+++ app/src/main/java/com/momocoffe/app/MainActivity.kt
@@ -2,7 +2,6 @@ package com.momocoffe.app
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -12,7 +11,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ProcessLifecycleOwner
 import com.momocoffe.app.navigation.NavigationScreen
-
 import com.momocoffe.app.ui.theme.MomoCoffeClientTheme
 import com.momocoffe.app.viewmodel.LoginViewModel
 import com.zettle.sdk.ZettleSDK
@@ -22,14 +20,9 @@ import com.zettle.sdk.feature.cardreader.ui.CardReaderFeature
 import com.zettle.sdk.feature.manualcardentry.ui.ManualCardEntryFeature
 import com.zettle.sdk.feature.qrc.paypal.PayPalQrcFeature
 import com.zettle.sdk.feature.qrc.venmo.VenmoQrcFeature
-import io.socket.client.IO
-import io.socket.client.Socket
-import io.socket.emitter.Emitter
-import java.net.URISyntaxException
 
 class MainActivity : ComponentActivity() {
     private val viewModel: LoginViewModel by viewModels()
-    private lateinit var mSocket: Socket
 
     fun initZettleSDK() {
         val clientId = getString(R.string.client_id)
