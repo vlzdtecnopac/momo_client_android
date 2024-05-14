ca48a13a91d6d686b82a3154d91e2443e20b0cad
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-05-14 09:53:14 -0500

    Update Momo Client

---
+++ app/src/main/java/com/momocoffe/app/ui/products/section/OptionsModifier.kt
@@ -0,0 +1,8 @@
+package com.momocoffe.app.ui.products.section
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OptionsModifier(){
+
+}
--- app/src/main/java/com/momocoffe/app/ui/products/section/Product.kt
+++ app/src/main/java/com/momocoffe/app/ui/products/section/Product.kt
@@ -1,14 +1,24 @@
 package com.momocoffe.app.ui.products.section
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
@@ -19,24 +29,60 @@ import com.momocoffe.app.ui.theme.BlueDark
 
 @Preview(widthDp = 1440, heightDp = 800)
 @Composable
-fun Product(){
+fun Product() {
 
     val navController = rememberNavController()
 
-    Column {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(BlueDark),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Header(navController)
-            Spacer(modifier = Modifier.height(8.dp))
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Header(navController)
+        Spacer(modifier = Modifier.height(8.dp))
+        Box(modifier = Modifier.background(BlueDark)) {
             Category(navController = navController)
-            Row {
-                DescriptionProduct()
+        }
+        Box(
+            modifier = Modifier
+                .widthIn(0.dp, 900.dp)
+        ) {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.White),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Row(modifier = Modifier.padding(20.dp)) {
+                        Column(
+                            modifier = Modifier
+                                .weight(.3f)
+                                .padding(10.dp)
+                        ) {
+                            DescriptionProduct()
+                        }
+                        Column(
+                            modifier = Modifier
+                                .weight(.8f)
+                                .fillMaxHeight()
+                                .clip(RoundedCornerShape(14.dp))
+                                .background(BlueDark)
+                                .border(
+                                    width = 1.2.dp,
+                                    color = BlueDark,
+                                    shape = RoundedCornerShape(14.dp)
+                                )
+                                .padding(10.dp),
+                            verticalArrangement = Arrangement.Top,
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            OptionsModifier()
+                        }
+
+                    }
 
             }
         }
     }
+
 }
