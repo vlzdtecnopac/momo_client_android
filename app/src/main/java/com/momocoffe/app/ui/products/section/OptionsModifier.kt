943128c9f9a6e1ebca19a8d19a34ee323d60b4af
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-05-15 00:42:35 -0500

    Update Android

--- app/src/main/java/com/momocoffe/app/ui/products/section/OptionsModifier.kt
+++ app/src/main/java/com/momocoffe/app/ui/products/section/OptionsModifier.kt
@@ -2,12 +2,13 @@ package com.momocoffe.app.ui.products.section
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -17,19 +18,34 @@ import com.momocoffe.app.ui.products.components.ItemBox
 import com.momocoffe.app.ui.products.components.ItemList
 import com.momocoffe.app.ui.products.components.ListOptions
 
-
 @Composable
 fun OptionsModifier() {
     val state = rememberScrollState()
-    LaunchedEffect(Unit) { state.animateScrollTo(100) }
 
     Column(
         modifier = Modifier
             .background(Color.Transparent)
-            .fillMaxSize()
+            .fillMaxWidth()
+            .fillMaxHeight(0.9f)
             .padding(horizontal = 8.dp)
             .verticalScroll(state)
     ) {
+        BoxOptions(
+            iconResource = R.drawable.tamano_icon,
+            textResource = R.string.size,
+            items = listOf(
+                ItemBox(1, "Chico 12 Oz", 10),
+                ItemBox(2, "Grande 16 Oz", 10),
+            )
+        )
+        BoxOptions(
+            iconResource = R.drawable.tamano_icon,
+            textResource = R.string.size,
+            items = listOf(
+                ItemBox(1, "Chico 12 Oz", 10),
+                ItemBox(2, "Grande 16 Oz", 10),
+            )
+        )
         BoxOptions(
             iconResource = R.drawable.tamano_icon,
             textResource = R.string.size,
--- app/src/main/java/com/momocoffe/app/ui/products/section/Product.kt
+++ app/src/main/java/com/momocoffe/app/ui/products/section/Product.kt
@@ -11,8 +11,13 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,12 +25,16 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.momocoffe.app.ui.components.Category
 import com.momocoffe.app.ui.components.Header
 import com.momocoffe.app.ui.products.components.DescriptionProduct
 import com.momocoffe.app.ui.theme.BlueDark
+import com.momocoffe.app.ui.theme.OrangeDark
+import com.momocoffe.app.ui.theme.OrangeDarkLight
+import com.momocoffe.app.ui.theme.redhatFamily
 
 @Composable
 fun Product(navController: NavController) {
@@ -76,9 +85,27 @@ fun Product(navController: NavController) {
                                 )
                                 .padding(10.dp),
                             verticalArrangement = Arrangement.Top,
-                            horizontalAlignment = Alignment.Start
+                            horizontalAlignment = Alignment.CenterHorizontally
                         ) {
                             OptionsModifier()
+                            Button(
+                                onClick = {},
+                                modifier = Modifier.width(400.dp).height(60.dp).padding(horizontal = 25.dp),
+                                shape =  RoundedCornerShape(8.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = OrangeDark,
+                                    disabledBackgroundColor = OrangeDarkLight,
+                                    disabledContentColor = OrangeDarkLight
+                                )
+                            ) {
+                                Text(
+                                    text = "Agregar al carrito",
+                                    fontSize = 22.sp,
+                                    color = Color.White,
+                                    fontFamily = redhatFamily,
+                                )
+                            }
+
                         }
 
                     }
