4913146f03ae19f7b8621ee47fc5099abf49ee88
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-05-21 22:43:57 -0500

    Update Android

--- app/src/main/java/com/momocoffe/app/ui/category/sections/Store.kt
+++ app/src/main/java/com/momocoffe/app/ui/category/sections/Store.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -33,11 +34,11 @@ import com.momocoffe.app.ui.theme.BlueDark
 fun Store(list: List<String>){
 
     val image = listOf(R.drawable.metch_icon, R.drawable.coffe_granel_icon)
-
+    val textLabel = listOf(R.string.text_merch, R.string.text_coffe_granel)
     val newList = mutableListOf<ListItem>()
 
     list.forEachIndexed { index, item ->
-        newList.add(ListItem(image[index], item))
+        newList.add(ListItem(image[index], stringResource(id =  textLabel[index])))
     }
 
 
