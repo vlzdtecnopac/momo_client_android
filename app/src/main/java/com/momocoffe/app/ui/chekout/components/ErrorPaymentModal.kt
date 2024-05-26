03ff36e0fdb833af44a457c808d13c7ca285cfac
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-05-26 01:58:26 -0500

    Update Android

---
+++ app/src/main/java/com/momocoffe/app/ui/chekout/components/ErrorPaymentModal.kt
@@ -0,0 +1,175 @@
+package com.momocoffe.app.ui.chekout.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.zIndex
+import com.momocoffe.app.ui.theme.BlueDark
+import com.momocoffe.app.ui.theme.BlueLight
+import com.momocoffe.app.ui.theme.OrangeDark
+import com.momocoffe.app.ui.theme.redhatFamily
+import com.momocoffe.app.R
+
+@Preview(widthDp = 1440, heightDp = 700, showBackground = true)
+@Composable
+fun ErrorPaymentModal(
+
+) {
+
+        Surface(
+            modifier = Modifier
+                .padding(0.dp)
+                .zIndex(88f),
+            color = BlueLight
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+                    .padding(10.dp)
+                    .background(BlueLight),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                var textState by rememberSaveable { mutableStateOf(value = "") }
+                val focusManager = LocalFocusManager.current
+
+                Image(
+                    painter = painterResource(id = R.drawable.momo_coffe_mug),
+                    contentDescription = stringResource(id = R.string.momo_coffe),
+                    modifier = Modifier.width(230.dp),
+                    contentScale = ContentScale.Crop,
+                )
+                Text(
+                    "¡Al parecer algo salió mal!",
+                    fontFamily = redhatFamily,
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight(700)
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    "Intentémoslo de nuevo.",
+                    fontFamily = redhatFamily,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight(700)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    "Tu pago no pudo ser procesado.\n" +
+                            "Por favor verifica tu tarjeta y vuelve a intentar,\n" +
+                            "si no lo logras, considera otra tarjeta o método de pago",
+                    fontFamily = redhatFamily,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight(400),
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    "¡Si hay algún problema, estamos para ayudarte!",
+                    fontFamily = redhatFamily,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight(400),
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Row(
+                    modifier = Modifier
+                        .widthIn(0.dp, 700.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .clip(RoundedCornerShape(14.dp))
+                            .border(
+                                width = 0.6.dp,
+                                color = BlueDark,
+                                shape = RoundedCornerShape(14.dp)
+                            )
+                            .weight(0.5f),
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(
+                            disabledContentColor = Color.Transparent,
+                            contentColor = Color.Transparent,
+                            backgroundColor = Color.Transparent
+                        ),
+                        elevation = ButtonDefaults.elevation(0.dp)
+                    ) {
+                        Text(
+                            "Cancelar",
+                            color = BlueDark,
+                            fontSize = 22.sp
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .weight(0.5f)
+                            .padding(horizontal = 5.dp)
+                            .border(
+                                width = 0.6.dp,
+                                color = OrangeDark,
+                                shape = RoundedCornerShape(14.dp)
+                            ),
+                        shape = RoundedCornerShape(14.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = OrangeDark,
+                            disabledBackgroundColor = OrangeDark,
+                            disabledContentColor = OrangeDark
+                        ),
+                        elevation = ButtonDefaults.elevation(0.dp)
+                    ) {
+                        Text(
+                            text = "Reintetar Pago",
+                            fontSize = 22.sp,
+                            color = Color.White,
+                            fontFamily = redhatFamily,
+                        )
+                    }
+
+                }
+            }
+        }
+
+
+
+}
