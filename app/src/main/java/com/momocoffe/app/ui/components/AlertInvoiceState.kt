4af49cf9476111369718b5042273817d5c35ed66
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-05-30 19:54:15 -0500

    Uptade Android

--- app/src/main/java/com/momocoffe/app/ui/components/AlertInvoiceState.kt
+++ app/src/main/java/com/momocoffe/app/ui/components/AlertInvoiceState.kt
@@ -38,6 +38,16 @@ import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.momocoffe.app.R
+import com.momocoffe.app.network.dto.Extra
+import com.momocoffe.app.network.dto.ExtraCoffee
+import com.momocoffe.app.network.dto.Lid
+import com.momocoffe.app.network.dto.Milk
+import com.momocoffe.app.network.dto.PedidoRequest
+import com.momocoffe.app.network.dto.Producto
+import com.momocoffe.app.network.dto.Size
+import com.momocoffe.app.network.dto.Sugar
+import com.momocoffe.app.network.dto.Temperature
+import com.momocoffe.app.network.dto.productosToString
 import com.momocoffe.app.ui.theme.BlueDark
 import com.momocoffe.app.ui.theme.BlueLight
 import com.momocoffe.app.ui.theme.OrangeDark
@@ -60,7 +70,34 @@ fun SuccessPaymentModal(
 ) {
 
     LaunchedEffect(Unit){
-        pedidoViewModel.create()
+        val pedidoData = PedidoRequest(
+            name_client = "Andres Gonzales",
+            shopping_id = "de86589e-255f-4987-8f6b-53845acfd154",
+            kiosko_id = "eae76186-6702-4216-8225-cee64e380f38",
+            columns_pending = 4,
+            product = productosToString(listOf(
+                Producto(
+                    id = "3caccd2e-9842-4239-aae2-a2a3d3cced5e",
+                    name_product = "Flat white",
+                    price = 40,
+                    image = "https://lh3.googleusercontent.com/q7eTMbvJ6FDmdCr24yHaA2Vgr4A62HTSUMPQ2A5hL3U4_oHM2KZyQ7OyhYz7cMm16kuIHBc3afRVw-lRRj01kjUD9G3Sh_TcKQ",
+                    extra = Extra(
+                        size = Size("Chico", 0),
+                        milk = Milk("Deslactosada", 5),
+                        sugar = Sugar("Blanca", 0),
+                        extra_coffee = listOf(ExtraCoffee("Extra shot No", "0")),
+                        lid = listOf(Lid("Sin Tapa", 0)),
+                        sauce = listOf(),
+                        temperature = Temperature("", 0),
+                        color = "",
+                        coffee_type = Any()
+                    ),
+                    quanty = 1,
+                    subtotal = 40
+                ),
+            ))
+        )
+        pedidoViewModel.create(pedidoData)
     }
 
     Dialog(
--- app/src/main/java/com/momocoffe/app/viewmodel/PedidoViewModel.kt
+++ app/src/main/java/com/momocoffe/app/viewmodel/PedidoViewModel.kt
@@ -24,36 +24,9 @@ class PedidoViewModel: ViewModel(){
     val loadingState = mutableStateOf(false)
     val pedidoResultState = mutableStateOf<Result<CreatePedidoResponse>?>(null)
 
-    fun create(){
+    fun create(pedidoData: PedidoRequest){
         loadingState.value = true
         viewModelScope.launch {
-            val pedidoData = PedidoRequest(
-                name_client = "Andres Gonzales",
-                shopping_id = "de86589e-255f-4987-8f6b-53845acfd154",
-                kiosko_id = "eae76186-6702-4216-8225-cee64e380f38",
-                columns_pending = 4,
-                product = productosToString(listOf(
-                    Producto(
-                        id = "3caccd2e-9842-4239-aae2-a2a3d3cced5e",
-                        name_product = "Flat white",
-                        price = 40,
-                        image = "https://lh3.googleusercontent.com/q7eTMbvJ6FDmdCr24yHaA2Vgr4A62HTSUMPQ2A5hL3U4_oHM2KZyQ7OyhYz7cMm16kuIHBc3afRVw-lRRj01kjUD9G3Sh_TcKQ",
-                        extra = Extra(
-                            size = Size("Chico", 0),
-                            milk = Milk("Deslactosada", 5),
-                            sugar = Sugar("Blanca", 0),
-                            extra_coffee = listOf(ExtraCoffee("Extra shot No", "0")),
-                            lid = listOf(Lid("Sin Tapa", 0)),
-                            sauce = listOf(),
-                            temperature = Temperature("", 0),
-                            color = "",
-                            coffee_type = Any()
-                        ),
-                        quanty = 1,
-                        subtotal = 40
-                    ),
-                ))
-            )
             try{
                 Log.e("Result.PedidoViewModel", pedidoData.toString() )
                 val response =  apiService.createPedido(pedidoData)
