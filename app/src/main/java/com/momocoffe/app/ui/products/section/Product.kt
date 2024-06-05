8a7511ef62641dc73fa056b2659c0303aeaeff0c
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-06-05 13:41:05 -0500

    Update Android

--- app/src/main/java/com/momocoffe/app/ui/products/section/Product.kt
+++ app/src/main/java/com/momocoffe/app/ui/products/section/Product.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -49,7 +50,9 @@ import com.momocoffe.app.ui.theme.redhatFamily
 import com.momocoffe.app.viewmodel.ProductsViewModel
 import com.momocoffe.app.R
 import com.momocoffe.app.network.data.CartProduct
+import com.momocoffe.app.ui.theme.BlueDarkTransparent
 import com.momocoffe.app.viewmodel.CartViewModel
+import com.spr.jetpack_loading.components.indicators.BallClipRotatePulseIndicator
 
 @Composable
 fun Product(
@@ -60,12 +63,17 @@ fun Product(
 ) {
 
     val context = LocalContext.current
+    val loading = productsViewModel.loadingState.value
     val sharedPreferences = context.getSharedPreferences("momo_prefs", Context.MODE_PRIVATE)
     val preference_shopping_id = sharedPreferences.getString("shoppingId", null) ?: ""
     var productsItems: List<ProductsItem>? by remember { mutableStateOf(null) }
     var selectProductID: String by remember { mutableStateOf("") }
 
     LaunchedEffect(Unit) {
+        productsViewModel.product(preference_shopping_id, product_id)
+    }
+
+    LaunchedEffect(selectProductID){
         productsViewModel.product(preference_shopping_id, product_id)
     }
 
@@ -86,134 +94,154 @@ fun Product(
         }
     }
 
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Header(navController, buttonExit = false)
-
+    Box {
         Column(
-            modifier = Modifier
-                .background(BlueDark)
-                .fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.height(8.dp))
-            Category(navController = navController, cartViewModel)
-            Spacer(modifier = Modifier.height(8.dp))
-        }
+            Header(navController, buttonExit = false)
 
-        productsItems.let { product ->
-
-            if( product?.get(0)?.productModifier?.tamaño ==  null){
-                productsViewModel.calculatePriceResult.value = product?.get(0)?.price?.toInt() ?: 0
-            }
-            Box(
+            Column(
                 modifier = Modifier
-                    .widthIn(0.dp, 900.dp)
+                    .background(BlueDark)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Category(navController = navController, cartViewModel)
+                Spacer(modifier = Modifier.height(8.dp))
+            }
 
-                Column(
+            productsItems.let { product ->
+
+                if (product?.get(0)?.productModifier?.tamaño == null) {
+                    productsViewModel.calculatePriceResult.value =
+                        product?.get(0)?.price?.toInt() ?: 0
+                }
+                Box(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.White),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .widthIn(0.dp, 900.dp)
                 ) {
 
-                    Row(modifier = Modifier.padding(10.dp)) {
-                        Column(
-                            modifier = Modifier
-                                .weight(.3f)
-                                .padding(end = 10.dp),
-                            verticalArrangement = Arrangement.Top
-                        ) {
-                            (product?.get(0) ?: null)?.let { DescriptionProduct(it) }
-                        }
-                        Column(
-                            modifier = Modifier
-                                .weight(.8f)
-                                .fillMaxHeight()
-                                .clip(RoundedCornerShape(14.dp))
-                                .background(BlueDark)
-                                .border(
-                                    width = 1.2.dp,
-                                    color = BlueDark,
-                                    shape = RoundedCornerShape(14.dp)
-                                )
-                                .padding(10.dp),
-                            verticalArrangement = Arrangement.Top,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            (product?.get(0) ?: null)?.let { OptionsModifier(it, onSelectID = {
-                                Log.d("Result.ProductsViewModel", it )
-                                selectProductID =  it
-                            }) }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.White),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        Row(modifier = Modifier.padding(10.dp)) {
                             Column(
+                                modifier = Modifier
+                                    .weight(.3f)
+                                    .padding(end = 10.dp),
+                                verticalArrangement = Arrangement.Top
+                            ) {
+                                (product?.get(0) ?: null)?.let { DescriptionProduct(it) }
+                            }
+                            Column(
+                                modifier = Modifier
+                                    .weight(.8f)
+                                    .fillMaxHeight()
+                                    .clip(RoundedCornerShape(14.dp))
+                                    .background(BlueDark)
+                                    .border(
+                                        width = 1.2.dp,
+                                        color = BlueDark,
+                                        shape = RoundedCornerShape(14.dp)
+                                    )
+                                    .padding(10.dp),
+                                verticalArrangement = Arrangement.Top,
                                 horizontalAlignment = Alignment.CenterHorizontally
                             ) {
-                                Spacer(modifier = Modifier.height(10.dp))
-                                Button(
-                                    onClick = {
-                                        Log.d("Result.ProductsViewModel", selectProductID.toString())
-                                        product?.get(0)?.let { firstProduct->
-                                            try {
-                                                val image = firstProduct.image ?: ""
-                                                cartViewModel.createProduct(
-                                                    CartProduct(
-                                                        "",
-                                                        productId = selectProductID,
-                                                        titleProduct = firstProduct.nameProduct,
-                                                        imageProduct = image,
-                                                        priceProduct = productsViewModel.calculatePriceResult.value.toString(),
-                                                        priceProductMod = productsViewModel.calculatePriceResult.value.toString(),
-                                                        countProduct = 1,
-                                                        modifiersOptions = productsViewModel.selectModifiersOptions,
-                                                        modifiersList = productsViewModel.selectModifiersList
-                                                    )
-                                                )
-                                                Toast.makeText(
-                                                    context,
-                                                    R.string.add_cart_product_new,
-                                                    Toast.LENGTH_LONG
-                                                )
-                                                    .show()
-                                            }catch (e: Exception){
-                                                Toast.makeText(
-                                                    context,
-                                                    R.string.error_cart_product_new,
-                                                    Toast.LENGTH_LONG
-                                                )
-                                                    .show()
-                                                Log.e("Result.CartModelView", e.message.toString())
-                                            }
-                                        }
-                                    },
-                                    modifier = Modifier
-                                        .width(400.dp)
-                                        .height(60.dp)
-                                        .padding(horizontal = 25.dp),
-                                    shape = RoundedCornerShape(8.dp),
-                                    colors = ButtonDefaults.buttonColors(
-                                        backgroundColor = OrangeDark,
-                                        disabledBackgroundColor = OrangeDarkLight,
-                                        disabledContentColor = OrangeDarkLight
-                                    )
-                                ) {
-                                    Text(
-                                        text = stringResource(id = R.string.add_cart_payment) + " $${productsViewModel.calculatePriceResult.value}",
-                                        fontSize = 18.sp,
-                                        color = Color.White,
-                                        fontFamily = redhatFamily,
-                                        fontWeight = FontWeight(700)
-                                    )
+                                (product?.get(0) ?: null)?.let {
+                                    OptionsModifier(it,
+                                        onSelectID = {
+                                            selectProductID = it
+                                        })
                                 }
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Spacer(modifier = Modifier.height(10.dp))
+                                    Button(
+                                        onClick = {
+                                            Log.d("Result.ProductsViewModel", selectProductID)
+                                            product?.get(0)?.let { firstProduct ->
+                                                try {
+                                                    val image = firstProduct.image ?: ""
+                                                    cartViewModel.createProduct(
+                                                        CartProduct(
+                                                            "",
+                                                            productId = selectProductID,
+                                                            titleProduct = firstProduct.nameProduct,
+                                                            imageProduct = image,
+                                                            priceProduct = productsViewModel.calculatePriceResult.value.toString(),
+                                                            priceProductMod = productsViewModel.calculatePriceResult.value.toString(),
+                                                            countProduct = 1,
+                                                            modifiersOptions = productsViewModel.selectModifiersOptions,
+                                                            modifiersList = productsViewModel.selectModifiersList
+                                                        )
+                                                    )
+                                                    Toast.makeText(
+                                                        context,
+                                                        R.string.add_cart_product_new,
+                                                        Toast.LENGTH_LONG
+                                                    )
+                                                        .show()
+                                                } catch (e: Exception) {
+                                                    Toast.makeText(
+                                                        context,
+                                                        R.string.error_cart_product_new,
+                                                        Toast.LENGTH_LONG
+                                                    )
+                                                        .show()
+                                                    Log.e(
+                                                        "Result.CartModelView",
+                                                        e.message.toString()
+                                                    )
+                                                }
+                                            }
+                                        },
+                                        modifier = Modifier
+                                            .width(400.dp)
+                                            .height(60.dp)
+                                            .padding(horizontal = 25.dp),
+                                        shape = RoundedCornerShape(8.dp),
+                                        colors = ButtonDefaults.buttonColors(
+                                            backgroundColor = OrangeDark,
+                                            disabledBackgroundColor = OrangeDarkLight,
+                                            disabledContentColor = OrangeDarkLight
+                                        )
+                                    ) {
+                                        Text(
+                                            text = stringResource(id = R.string.add_cart_payment) + " $${productsViewModel.calculatePriceResult.value}",
+                                            fontSize = 18.sp,
+                                            color = Color.White,
+                                            fontFamily = redhatFamily,
+                                            fontWeight = FontWeight(700)
+                                        )
+                                    }
+                                }
+
                             }
 
                         }
 
                     }
-
                 }
+            }
+
+        }
+
+        if (loading) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(BlueDarkTransparent)
+            ) {
+                BallClipRotatePulseIndicator()
             }
         }
 
